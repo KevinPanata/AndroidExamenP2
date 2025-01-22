2865bd72af2aa7a32e59cd28359838c5252f89dc
KEVIN IVAN PANATA VASQUEZ <121053809+KevinPanata@users.noreply.github.com>
2025-01-22 18:28:16 -0500

    Delete Biblioteca directory

--- Biblioteca/src/main/java/com/example/Biblioteca/repositorio/LibroRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.example.Biblioteca.repositorio;
-
-import com.example.Biblioteca.modelo.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibroRepositorio extends JpaRepository<Libro, Long> {
-}
