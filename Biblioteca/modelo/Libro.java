82b76a6256fe57f26150774d3712cf84748b77ea
KEVIN IVAN PANATA VASQUEZ <121053809+KevinPanata@users.noreply.github.com>
2025-01-22 18:17:57 -0500

    Add files via upload

---
+++ Biblioteca/modelo/Libro.java
@@ -0,0 +1,52 @@
+package com.example.Biblioteca.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Libro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String titulo;
+    private String autor;
+
+    // Constructor vacío
+    public Libro() {
+    }
+
+    // Constructor con parámetros
+    public Libro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    // Getters y Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+}
